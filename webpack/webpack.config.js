3dfb015aaee6dc414ea0b800a78eba2946e50db9
Cryden <cryden_da@mail.ru>
2018-04-08 02:30:29 +0300

    add fonts support

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -37,6 +37,17 @@ module.exports = {
           }],
           fallback: 'style-loader'
         })
+      },
+      {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+            publicPath: './../fonts/'
+          }
+        }]
       }
     ]
   },
