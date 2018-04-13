30bca0f0c5aec415724bbbe3037b028aed25defe
CRYDEsigN <Cryden_da@mail.ru>
2018-04-13 11:41:21 +0300

    config refactory

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-const config = require('node-config-yaml').load('./webpack/config.yml')
+const config = require('node-config-yaml').load('./webpack/configs')
 const path = require('path')
 
-// console.log(config)
-let cf = {}
-let entry = {}
-function addEntry (config) {
-  config.entry.javascript.forEach(function (item) {
-    entry[item] = path.join(config.path.source, config.path.entry, item + '.js')
-  })
-  cf.entry = entry
-}
-
-addEntry(config)
-
-console.log(cf)
+console.log(config)
